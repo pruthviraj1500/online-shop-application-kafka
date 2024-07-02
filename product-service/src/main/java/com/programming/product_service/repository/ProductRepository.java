d1f4a92ed05e08513f18c12a3a8be4e58a98744e
63606_Pruthviraj <pruthvirajab1008@gmail.com>
2024-07-02 15:12:45 +0530

    Product service Test cases added

---
+++ product-service/src/main/java/com/programming/product_service/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.programming.product_service.repository;
+
+import com.programming.product_service.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product,String> {
+
+
+
+}
